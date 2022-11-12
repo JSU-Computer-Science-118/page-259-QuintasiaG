afde6390d8fb4b2fc8a98197106a0adb94d495ce
QuintasiaG <117471248+QuintasiaG@users.noreply.github.com>
2022-11-11 23:18:34 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,19 +21,19 @@ int main()
              }
    
     discriminant = (pow(b,2.0)-4*a*c) ;
-        root1 = (-b -sqrt(discriminant)) / (2 * a);
-        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        root1 = (-b -sqrt(pow(b,2.0)-4*a*c)) / (2 * a);
+        root2 = (-b - sqrt(pow(b,2.0)-4*a*c)) / (2 * a);
     
     if (discriminant==0) 
         cout << "The single root is" << root1<<endl;
      if (discriminant>0) 
         cout << "Two real roots" << endl;
       if (discriminant>0) 
-     cout << "First root" << (-b) / (2 * a) << root1<<endl;
+     cout << "- First root" << (-b) / (2 * a) << root1<<endl;
 
        
     if  (discriminant>0) 
-       cout << "Second root" <<root2<<endl;
+       cout << "- Second root" <<root2<<endl;
         
         cout << "Roots are " << root1 << " and " << root2 << endl;
    if (discriminant<0) 
