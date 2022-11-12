d645740b76252ece2207fce64f026c36312e391a
QuintasiaG <117471248+QuintasiaG@users.noreply.github.com>
2022-11-11 22:23:50 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,23 +9,26 @@ using namespace std;
 int main()
 {
     double a, b, c, discriminant, root1, root2;
-    cout << "Enter a, b and c in the quadratic equation ax^2 + bx + c = 0: ";
-    
+  
     discriminant = b * b - 4 * a * c;
    
     if (a == 0 || discriminant < 0) {
         cout << "The single root is" << root1<<endl;
-    } else if (discriminant == 0) {
+     if (discriminant == 0) {
         cout << "Two real roots" << endl;
         cout << "First root" << (-b) / (2 * a) << root1<<endl;
 
        
-    } else {
+    } if {
         cout << "Second root" <<root2<<endl;
-        discriminant = sqrt(discriminant);
+        discriminant = (pow(b,2,0])-4*a*c;
         root1 = (-b + discriminant) / (2 * a);
         root2 = (-b - discriminant) / (2 * a);
         cout << "Roots are " << root1 << " and " << root2 << endl;
-    }
+   if (discriminant == 0) {
+       cout << "Complex roots" << endl
+     }
+        
+    
     return 0;
 }
