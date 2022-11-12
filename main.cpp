f22f63a9cf064275b135d4213086cc4cb9a56994
QuintasiaG <117471248+QuintasiaG@users.noreply.github.com>
2022-11-11 23:15:34 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,32 +2,41 @@
 
 #include <iostream>
 #include <cmath>
-
-
 using namespace std;
 
 int main()
 {
     double a, b, c, discriminant, root1, root2;
+    
+    cin>>a;
+    cin>>b;
+    cin>>c;
   
     discriminant = b * b - 4 * a * c;
+    
+    if(a==0)
+       {
+           cout << "Invalid Entry" << endl;
+            return 0;
+             }
    
-    discriminant = (pow(b,2.0))-4*a*c;
+    discriminant = (pow(b,2.0)-4*a*c) ;
         root1 = (-b -sqrt(discriminant)) / (2 * a);
         root2 = (-b - sqrt(discriminant)) / (2 * a);
     
-    if (a == 0 || discriminant < 0) 
+    if (discriminant==0) 
         cout << "The single root is" << root1<<endl;
-     if (discriminant == 0) 
+     if (discriminant>0) 
         cout << "Two real roots" << endl;
-        cout << "First root" << (-b) / (2 * a) << root1<<endl;
+      if (discriminant>0) 
+     cout << "First root" << (-b) / (2 * a) << root1<<endl;
 
        
-    if  (discriminant == 0) 
+    if  (discriminant>0) 
        cout << "Second root" <<root2<<endl;
         
         cout << "Roots are " << root1 << " and " << root2 << endl;
-   if (discriminant == 0) 
+   if (discriminant<0) 
        cout << "Complex roots" << endl;
      
         
@@ -35,6 +44,15 @@ int main()
     return 0; 
     }
         
+    
+
+ 
+    
+
+
+    
+
+
     
 
  
