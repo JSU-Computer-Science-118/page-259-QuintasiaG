832d6d1c77f7d74ce71c614408f124e7eab332c7
QuintasiaG <117471248+QuintasiaG@users.noreply.github.com>
2022-11-11 22:15:04 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,31 @@
 // Quintasia Gordon J00967930 10-11-2022 DONE
 
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
+
+
 using namespace std;
 
-int main(){
-	float a,b,c;
-	cout<<"Enter coefficient of x*x: ";
-	cin>>a;
-	cout<<"Enter coefficient of x: ";
-	cin>>b;
-	cout<<"Enter the constant term: ";
-	cin>>c;
-	float dis;
-	dis=(b*b-4*a*c);//calculating the discriminant 
-	if(dis<0){
-		cout<<"The equation has two complex roots"<<endl;
-	}else if(dis==0){
-		cout<<"The equation has a single (repeated) root"<<endl;
-		float x1,x2;
-		x1=(-b + sqrt(dis))/(2*a);//calculating the repeated root
-		x2=x1;
-		cout<<"Root is "<<x1<<endl;
-	}else{
-		cout<<"The equation has two real roots"<<endl;
-		float x1,x2;
-		x1=(-b + sqrt(dis))/(2*a);//calculating the two real roots
-		x2=(-b - sqrt(dis))/(2*a);
-		cout<<"Roots are "<<x1<<" and "<<x2<<endl;
-	}
-	return 0;
+int main()
+{
+    double a, b, c, discriminant, root1, root2;
+    cout << "Enter a, b and c in the quadratic equation ax^2 + bx + c = 0: ";
+    
+    discriminant = b * b - 4 * a * c;
+   
+    if (a == 0 || discriminant < 0) {
+        cout << "The single root is" << root1<<endl;
+    } else if (discriminant == 0) {
+        cout << "Two real roots" << endl;
+        cout << "First root" << (-b) / (2 * a) << root1<<endl;
+
+       
+    } else {
+        cout << "Second root" <<root2<<endl;
+        discriminant = sqrt(discriminant);
+        root1 = (-b + discriminant) / (2 * a);
+        root2 = (-b - discriminant) / (2 * a);
+        cout << "Roots are " << root1 << " and " << root2 << endl;
+    }
+    return 0;
 }
