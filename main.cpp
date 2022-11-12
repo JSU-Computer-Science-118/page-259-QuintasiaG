d572c7797ad766c38979ff0fc294d6df7b53b913
QuintasiaG <117471248+QuintasiaG@users.noreply.github.com>
2022-11-11 21:52:52 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,80 +1,33 @@
 // Quintasia Gordon J00967930 10-11-2022 DONE
 
-#include <iostream>
-#include <cmath>
-#include <iomanip>
-
-
+#include<iostream>
+#include<cmath>
 using namespace std;
 
-
-int main() 
-{
-  
-    int a, b, c;
-    double disc = 0;
-    double root1, root2;
-
-
-    cout << "Enter the value of a (the coefficient of x^2): ";
-
-    cin >> a;
-
- 
-    cout << "Enter the value of b (the coefficient of x): ";
-
- 
-    cin >> b;
-
-    cout << "Enter the value of c (the constant term): ";
-
-
-    cin >> c;
-
-    cout << fixed << setprecision(2);
-
-
-    disc = (b*b) - 4*(a*c);
-
-
-    if(disc < 0)
-    {
-        cout << endl 
-            << "The equation has two complex roots." 
-            << endl;
-    }
-
-
-    else if(disc == 0)
-    {
-        cout << endl 
-            << "The equation has a single (repeated) root." 
-            << endl;
-
-  
-        root1 = (0-b)/(double)(2*a);
-        
- 
-        cout << "Root 1 = Root 2 = " << root1 << endl;
-    }
-
- 
-    else
-    {
-        cout << endl 
-            << "The equation has two real roots." 
-            << endl;
-
-        root1 = ((0-b) + pow(disc, 0.5))/(double)(2*a);
-
-        root2 = ((0-b) - pow(disc, 0.5))/(double)(2*a);
-
-   
-        cout << "Root 1 = " << root1 << endl;
-        cout << "Root 2 = " << root2 << endl;
-    }
-
- 
-    return 0;
+int main(){
+	float a,b,c;
+	cout<<"Enter coefficient of x*x: ";
+	cin>>a;
+	cout<<"Enter coefficient of x: ";
+	cin>>b;
+	cout<<"Enter the constant term: ";
+	cin>>c;
+	float dis;
+	dis=(b*b-4*a*c);//calculating the discriminant 
+	if(dis<0){
+		cout<<"The equation has two complex roots"<<endl;
+	}else if(dis==0){
+		cout<<"The equation has a single (repeated) root"<<endl;
+		float x1,x2;
+		x1=(-b + sqrt(dis))/(2*a);//calculating the repeated root
+		x2=x1;
+		cout<<"Root is "<<x1<<endl;
+	}else{
+		cout<<"The equation has two real roots"<<endl;
+		float x1,x2;
+		x1=(-b + sqrt(dis))/(2*a);//calculating the two real roots
+		x2=(-b - sqrt(dis))/(2*a);
+		cout<<"Roots are "<<x1<<" and "<<x2<<endl;
+	}
+	return 0;
 }
-
